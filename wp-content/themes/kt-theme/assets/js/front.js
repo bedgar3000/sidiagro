6f421b7876689f4c99799371b32d3d4471bc953c
Edgar Bolivar <bedgar3000@gmail.com>
2022-09-21 17:38:46 -0400

    ...

---
+++ wp-content/themes/kt-theme/assets/js/front.js
@@ -0,0 +1,54 @@
+(function ($) {
+    "use strict";
+
+    $(function () {
+        $(document).ready(function () {
+            //  pagina archive de proveedores
+            if ($('#form-fito').length) {
+                //  filtro condado
+                $('#form-fito').on('submit', function (e) {
+                    e.preventDefault();
+                    handleSubmit();
+                });
+            }
+        });
+
+        /**
+         * Submit form
+         */
+        function handleSubmit() {
+            var formData = new FormData(document.getElementById('form-fito'));
+            formData.append('action', 'form_fito');
+
+            // let overlay = document.getElementsByClassName('loading-overlay')[0];
+            // overlay.classList.toggle('is-active');
+
+            $.ajax({
+                type: "POST",
+                url: front_ajax_obj.ajaxurl,
+                data: formData,
+                dataType: "JSON",
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    // overlay.classList.toggle('is-active');
+                    console.log(response);
+
+                    if (response.status == 'ERROR') {
+                        response.error_data.forEach(function (error) {
+                            //create message error in input get getElementsByName
+                            var input = document.getElementsByName(error.input)[0];
+                            var message = document.createElement('p');
+                            message.classList.add('text-danger');
+                            message.innerHTML = error.message;
+                            input.parentNode.appendChild(message);
+                        });
+                    } else {
+                        alert(response.status);
+                    }
+                }
+            });
+        }
+    });
+
+})(jQuery);
